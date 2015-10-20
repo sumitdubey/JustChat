7601a4b99305944560f87241036bfd8a2e43ef85
sumitdubey <sumitdube1991@gmail.com>
2015-10-20 19:34:38 +0530

    added support to maintain list for user
    - added support to maintain list for user
    - new user event handeling

--- server.js
+++ server.js
@@ -4,13 +4,19 @@ var io = require('socket.io')(http);
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
-
+var userList = [];
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
+  socket.on('new user', function(userName){
+    userList.push(userName);
+    io.emit('user list', userList);
+  });
+  
+  socket.on('chat message', function(payload){
+    //payload = JSON.parse(payload);
     io.emit('chat message', msg);
   });
 });
