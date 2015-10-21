b363a3d9c6be682db0db839f9157df8939a67796
sumit <sdubey@innodata.com>
2015-10-21 20:05:19 +0530

    - fixed some bug

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '10.120.57.18'
 var userList = [];
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -17,7 +17,7 @@ io.on('connection', function(socket){
   
   socket.on('chat message', function(payload){
     //payload = JSON.parse(payload);
-    io.emit('chat message', msg);
+    io.emit('chat message', payload);
   });
 });
 
